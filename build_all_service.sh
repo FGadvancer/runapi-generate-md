3c025dd0c2ac6fe0e21e451bde14fb4768babaf3
Gordon <1432970085@qq.com>
2023-03-17 18:11:30 +0800

    tools

---
+++ build_all_service.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+
+
+bin_dir="./bin"
+#Automatically created when there is no bin, logs folder
+if [ ! -d $bin_dir ]; then
+  mkdir -p $bin_dir
+
+#begin path
+begin_path=$PWD
+
+service_source_root=(
+  ./cmd/
+)
+
+for ((i = 0; i < ${#service_source_root[*]}; i++)); do
+  cd $begin_path
+  service_path=${service_source_root[$i]}
+  cd $service_path
+  make install
+  if [ $? -ne 0 ]; then
+        echo -e "${RED_PREFIX}${service_names[$i]} build failed ${COLOR_SUFFIX}\n"
+        exit -1
+        else
+         echo -e "${GREEN_PREFIX}${service_names[$i]} successfully be built ${COLOR_SUFFIX}\n"
+  fi
+done
+echo -e ${YELLOW_PREFIX}"all services build success"${COLOR_SUFFIX}
