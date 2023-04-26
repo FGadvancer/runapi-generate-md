a08ef2c339c4b9d84b93a9cdd00df829bbc527b7
Gordon <1432970085@qq.com>
2023-04-27 01:51:37 +0800

    recursion gen update

--- cmd/main.go
+++ cmd/main.go
@@ -4,23 +4,20 @@ import (
 	"flag"
 	"fmt"
 	"github.com/ruapi-generate-md/internal"
-	"os"
-	"os/exec"
-	"path/filepath"
 	"time"
 )
 
 func main() {
 	t := time.Now()
 	fmt.Println("generate start at ", t)
-	path, err := exec.LookPath(os.Args[0])
-	if err != nil {
-		panic(err)
-	}
-	dir := filepath.Dir(path)
-	parentDir := filepath.Dir(dir)
+	//path, err := exec.LookPath(os.Args[0])
+	//if err != nil {
+	//	panic(err)
+	//}
+	//dir := filepath.Dir(path)
+	//parentDir := filepath.Dir(dir)
 
-	fmt.Printf("当前代码执行目录的上一级目录为：%s\n", parentDir)
+	//fmt.Printf("当前代码执行目录的上一级目录为：%s\n", parentDir)
 	projectName := flag.String("p", "ServerAPI", "api project name")
 	flag.Parse()
 	internal.GeneratePageByItemID("..", *projectName)
