ac8d0a4677ee629e7fa030059b8296e86a2692ed
Gordon <1432970085@qq.com>
2023-07-04 20:47:21 +0800

    recursion gen update

--- internal/single_catalog_generator.go
+++ internal/single_catalog_generator.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"fmt"
 	"github.com/ruapi-generate-md/pkg"
+	"github.com/ruapi-generate-md/pkg/constant"
 	"os"
 	"strings"
 )
@@ -158,10 +159,8 @@ func generateOnePageMarkDown(jsonStr string, globalHeader []pkg.Header, bigTile 
 	//jsonStr := "{&quot;info&quot;:{&quot;from&quot;:&quot;runapi&quot;,&quot;type&quot;:&quot;api&quot;,&quot;title&quot;:&quot;&quot;,&quot;description&quot;:&quot;获取用户token&quot;,&quot;method&quot;:&quot;post&quot;,&quot;url&quot;:&quot;{{host}}/auth/user_token&quot;,&quot;remark&quot;:&quot;&quot;,&quot;apiStatus&quot;:&quot;3&quot;},&quot;request&quot;:{&quot;params&quot;:{&quot;mode&quot;:&quot;json&quot;,&quot;urlencoded&quot;:[{&quot;name&quot;:&quot;&quot;,&quot;type&quot;:&quot;string&quot;,&quot;value&quot;:&quot;&quot;,&quot;require&quot;:&quot;1&quot;,&quot;remark&quot;:&quot;&quot;}],&quot;formdata&quot;:[{&quot;name&quot;:&quot;&quot;,&quot;type&quot;:&quot;string&quot;,&quot;value&quot;:&quot;&quot;,&quot;require&quot;:&quot;1&quot;,&quot;remark&quot;:&quot;&quot;}],&quot;json&quot;:&quot;{\\n  \\&quot;userID\\&quot;: \\&quot;635204331\\&quot;,\\n  \\&quot;platformID\\&quot;: 1,\\n  \\&quot;secret\\&quot;:\\&quot;openIM123\\&quot;\\n}&quot;,&quot;jsonDesc&quot;:[{&quot;name&quot;:&quot;userID&quot;,&quot;type&quot;:&quot;string&quot;,&quot;remark&quot;:&quot;&quot;,&quot;value&quot;:&quot;&quot;,&quot;require&quot;:&quot;1&quot;},{&quot;name&quot;:&quot;platformID&quot;,&quot;type&quot;:&quot;string&quot;,&quot;remark&quot;:&quot;&quot;,&quot;value&quot;:&quot;&quot;,&quot;require&quot;:&quot;1&quot;},{&quot;name&quot;:&quot;secret&quot;,&quot;type&quot;:&quot;string&quot;,&quot;remark&quot;:&quot;&quot;,&quot;value&quot;:&quot;&quot;,&quot;require&quot;:&quot;1&quot;}]},&quot;headers&quot;:[{&quot;name&quot;:&quot;operationID&quot;,&quot;type&quot;:&quot;string&quot;,&quot;value&quot;:&quot;123123123&quot;,&quot;require&quot;:&quot;1&quot;,&quot;remark&quot;:&quot;operationID用于全局链路追踪&quot;}],&quot;cookies&quot;:[{&quot;name&quot;:&quot;&quot;,&quot;value&quot;:&quot;&quot;}],&quot;auth&quot;:[],&quot;query&quot;:[{&quot;name&quot;:&quot;&quot;,&quot;type&quot;:&quot;string&quot;,&quot;value&quot;:&quot;&quot;,&quot;require&quot;:&quot;1&quot;,&quot;remark&quot;:&quot;&quot;}],&quot;pathVariable&quot;:[]},&quot;response&quot;:{&quot;responseText&quot;:&quot;{\\n  \\&quot;errCode\\&quot;: 0,\\n  \\&quot;errMsg\\&quot;: \\&quot;\\&quot;,\\n  \\&quot;errDlt\\&quot;: \\&quot;\\&quot;,\\n  \\&quot;data\\&quot;: {\\n    \\&quot;token\\&quot;: \\&quot;eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVSUQiOiI2MzUyMDQzMzEiLCJQbGF0Zm9ybSI6IklPUyIsImV4cCI6MTY4NjU2MTkxMSwibmJmIjoxNjc4Nzg1NjExLCJpYXQiOjE2Nzg3ODU5MTF9.uRSAZE_Z-FlBFjzuLp4Usy2utT-BQLR1eIzeaS9nCJk\\&quot;,\\n    \\&quot;expireTimeSeconds\\&quot;: 90\\n  }\\n}&quot;,&quot;responseOriginal&quot;:{&quot;errCode&quot;:0,&quot;errMsg&quot;:&quot;&quot;,&quot;errDlt&quot;:&quot;&quot;,&quot;data&quot;:{&quot;token&quot;:&quot;eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVSUQiOiI2MzUyMDQzMzEiLCJQbGF0Zm9ybSI6IklPUyIsImV4cCI6MTY4NjU2MTkxMSwibmJmIjoxNjc4Nzg1NjExLCJpYXQiOjE2Nzg3ODU5MTF9.uRSAZE_Z-FlBFjzuLp4Usy2utT-BQLR1eIzeaS9nCJk&quot;,&quot;expireTimeSeconds&quot;:90}},&quot;responseExample&quot;:&quot;{\\n  \\&quot;errCode\\&quot;: 0,\\n  \\&quot;errMsg\\&quot;: \\&quot;\\&quot;,\\n  \\&quot;errDlt\\&quot;: \\&quot;\\&quot;,\\n  \\&quot;data\\&quot;: {\\n    \\&quot;token\\&quot;: \\&quot;eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJVSUQiOiI2MzUyMDQzMzEiLCJQbGF0Zm9ybSI6IklPUyIsImV4cCI6MTY4NjU2MTI2OCwibmJmIjoxNjc4Nzg0OTY4LCJpYXQiOjE2Nzg3ODUyNjh9.bypHmiNJtIcpxxCOcA-cWNe0ymyp4-Sa80pmtpF5G0s\\&quot;,\\n    \\&quot;expireTimeSeconds\\&quot;: 90\\n  }\\n}&quot;,&quot;responseHeader&quot;:{&quot;access-control-allow-credentials&quot;:&quot;false&quot;,&quot;access-control-allow-headers&quot;:&quot;*&quot;,&quot;access-control-allow-methods&quot;:&quot;*&quot;,&quot;access-control-allow-origin&quot;:&quot;*&quot;,&quot;access-control-expose-headers&quot;:&quot;Content-Length, Access-Control-Allow-Origin, Access-Control-Allow-Headers,Cache-Control,Content-Language,Content-Type,Expires,Last-Modified,Pragma,FooBar&quot;,&quot;access-control-max-age&quot;:&quot;172800&quot;,&quot;content-length&quot;:&quot;277&quot;,&quot;content-type&quot;:&quot;application/json&quot;,&quot;cookie-from-server&quot;:&quot;&quot;,&quot;date&quot;:&quot;Tue, 14 Mar 2023 09:25:11 GMT&quot;},&quot;responseStatus&quot;:200,&quot;responseTime&quot;:162,&quot;responseParamsDesc&quot;:[{&quot;name&quot;:&quot;errCode&quot;,&quot;type&quot;:&quot;string&quot;,&quot;remark&quot;:&quot;&quot;},{&quot;name&quot;:&quot;errMsg&quot;,&quot;type&quot;:&quot;string&quot;,&quot;remark&quot;:&quot;&quot;},{&quot;name&quot;:&quot;errDlt&quot;,&quot;type&quot;:&quot;string&quot;,&quot;remark&quot;:&quot;&quot;},{&quot;name&quot;:&quot;data&quot;,&quot;type&quot;:&quot;object&quot;,&quot;remark&quot;:&quot;&quot;},{&quot;name&quot;:&quot;token&quot;,&quot;type&quot;:&quot;string&quot;,&quot;remark&quot;:&quot;&quot;},{&quot;name&quot;:&quot;expireTimeSeconds&quot;,&quot;type&quot;:&quot;string&quot;,&quot;remark&quot;:&quot;&quot;}],&quot;responseFailExample&quot;:&quot;{\\n  \\&quot;errCode\\&quot;: 10000,\\n  \\&quot;errMsg\\&quot;: \\&quot;: [12]unknown service OpenIMServer.pbAuth.Auth\\&quot;,\\n  \\&quot;errDlt\\&quot;: \\&quot;\\&quot;,\\n  \\&quot;data\\&quot;: null\\n}&quot;,&quot;responseFailParamsDesc&quot;:[{&quot;name&quot;:&quot;errCode&quot;,&quot;type&quot;:&quot;string&quot;,&quot;remark&quot;:&quot;&quot;},{&quot;name&quot;:&quot;errMsg&quot;,&quot;type&quot;:&quot;string&quot;,&quot;remark&quot;:&quot;&quot;},{&quot;name&quot;:&quot;errDlt&quot;,&quot;type&quot;:&quot;string&quot;,&quot;remark&quot;:&quot;&quot;},{&quot;name&quot;:&quot;data&quot;,&quot;type&quot;:&quot;object&quot;,&quot;remark&quot;:&quot;&quot;}],&quot;remark&quot;:&quot;&quot;,&quot;responseSize&quot;:0},&quot;scripts&quot;:{&quot;pre&quot;:&quot;&quot;,&quot;post&quot;:&quot;&quot;},&quot;extend&quot;:{}}"
 	jsonStr = strings.ReplaceAll(jsonStr, "&quot;", "\"")
 	//fmt.Println("11111", jsonStr)
-	omitEmptyMap := []string{"SDK-Core编译文档", "OpenIM文档撰写说明以及参数"}
-
-	if IsContain(bigTile, omitEmptyMap) {
-		return
+	if _, ok := constant.Exclude[bigTile]; ok {
+		fmt.Println("exclude:", bigTile)
 	}
 	data := pkg.PageContent{}
 	err := json.Unmarshal([]byte(jsonStr), &data)
@@ -175,6 +174,7 @@ func generateOnePageMarkDown(jsonStr string, globalHeader []pkg.Header, bigTile 
 			"<style>\n" +
 			"th {\n    background-color: #1E90FF; /* 设置表头背景颜色 */\n}\n.highlight {\n    background-color: black;\n    color: white;\n    font-family: Consolas, Monaco, 'Andale Mono', 'Ubuntu Mono', monospace;\n}\n</style>" + "\n\n")
 	}
+	sb.WriteString("---\ntitle: " + bigTile + "\nhide_title: true\n---\n\n")
 	pageBigTitleWrite(&sb, bigTile, 2)
 	//fmt.Println("1", data.Request.Params.JSON, data.Request.Headers)
 	titleWrite(&sb, "简要描述", 3)
@@ -202,9 +202,16 @@ func generateOnePageMarkDown(jsonStr string, globalHeader []pkg.Header, bigTile 
 	codeAreaWrite(&sb, data.Response.ResponseFailExample)
 	titleWrite(&sb, "失败返回示例的参数说明\n\n", 3)
 	failedRespTableWrite(&sb, data.Response.ResponseFailParamsDesc)
-	err = os.WriteFile(catalogPath+"/"+bigTile+".md", []byte(sb.String()), 0644)
+	err = os.WriteFile(catalogPath+"/"+tileChanged(bigTile)+".mdx", []byte(sb.String()), 0644)
 	//err = ioutil.WriteFile(bigTile+".md", []byte(sb.String()), 0644)
 	if err != nil {
 		fmt.Println("WriteFile err", err, "catlogPath", catalogPath, "bigTile", bigTile)
+	}
+}
+func tileChanged(bigTile string) string {
+	if v, ok := constant.FileName[bigTile]; ok {
+		return v
+	} else {
+		return bigTile
 	}
 }
---
+++ pkg/constant/constant.go
@@ -0,0 +1,48 @@
+package constant
+
+var FileName = map[string]string{
+	"会话相关": "conversationManagement",
+	"好友管理": "friendsManagement",
+	"群组管理": "groupManagement",
+	"用户管理": "userManagement",
+
+	"消息相关":        "messageManagement",
+	"三方服务":        "thirdManagement",
+	"撤回消息":        "revokeMessage",
+	"删除用户所有消息":    "deleteUserAllMessage",
+	"发送消息":        "sendMessage",
+	"用户注册":        "userRegister",
+	"分页获取已注册用户列表": "getUserList",
+	"设置用户全局免打扰":   "updateGlobalRemind",
+	"获取指定用户列表":    "getSpecifiedUser",
+	"更新用户信息":      "updateUserInfo",
+	"获取用户token":   "getUserToken",
+	"查询用户是否注册":    "checkUserRegistered",
+	"分页获取所有用户ID":  "getAllUserID",
+	"获取用户在线状态":    "getUserOnlineStatus",
+
+	"判断是否是好友":       "checkFriend",
+	"获取收到的好友申请列表":   "getRecvApplication",
+	"移除黑名单列表":       "deleteBlackList",
+	"获取好友信息列表":      "getFriendList",
+	"设置好友备注":        "updateFriendRemark",
+	"用户申请加好友":       "sendApplication",
+	"删除好友":          "deleteFriend",
+	"获取用户黑名单列表":     "getBlackList",
+	"添加黑名单":         "addBlackList",
+	"导入好友":          "importFriend",
+	"好友申请处理":        "processApplication",
+	"获取主动发出的好友申请列表": "getSentApplication",
+}
+var Exclude = map[string]struct{}{
+	"获取最新seq":                   {},
+	"通过seq拉取消息":                 {},
+	"用户清空指定会话ID消息":              {},
+	"用户删除会话指定消息":                {},
+	"按时间物理删除消息":                 {},
+	"按seq物理删除消息":                {},
+	"获取会话最大seq和用户已读最大seq":       {},
+	"标记seq列表中消息已读并且发送已读回执同步未读数": {},
+	"SDK-Core编译文档":              {},
+	"OpenIM文档撰写说明以及参数":          {},
+}
